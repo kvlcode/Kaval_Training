a650dbd02ac25fe2cd0180770bc80ba5d147171f
kvlcode <kavalprajapati126@gmail.com>
2022-01-18 13:03:54 +0530

    Added Day11 SQL file

---
+++ SQL/Day11_SQL.sql
@@ -0,0 +1,108 @@
+/*DAY-11 INSERT, UPDATE, DELETE*/
+
+USE database1;
+
+CREATE TABLE employees (
+    emp_no      INT             NOT NULL,
+    birth_date  DATE            NOT NULL,
+    first_name  VARCHAR(14)     NOT NULL,
+    last_name   VARCHAR(16)     NOT NULL,
+    gender      ENUM ('M','F')  NOT NULL,    
+    hire_date   DATE            NOT NULL,
+    PRIMARY KEY (emp_no)
+);
+
+/*INSERT STATEMENT*/
+INSERT INTO employees 
+(	
+		emp_no, 
+        birth_date,
+        first_name,
+        last_name,
+        gender, 
+        hire_date
+)VALUES
+(
+		1,
+        '1990-01-01',
+        'John',
+        'Smith',
+        'M',
+        '2010-01-01'
+);       
+
+/*INSERT in different order of values*/ 
+/*MUST PUT THE VALUES IN EXACT ORDER WE HAVE LISTED THE COLUMN NAMES*/
+
+INSERT INTO employees 
+(	
+        birth_date,
+        emp_no,
+        first_name,
+        last_name,
+        gender, 
+        hire_date
+)VALUES
+(
+		'1995-02-02',
+		 2,
+        'Ruby',
+        'Smith',
+        'F',
+        '2005-05-05'
+); 
+
+
+INSERT INTO employees
+VALUES
+(
+		3,
+        '1980-08-01',
+        'Bob',
+        'Marlie',
+        'M',
+        '2001-05-04'
+); 
+SELECT * FROM employees;
+
+
+/* Copy table1 values in table2 using Insert */
+
+CREATE TABLE departments (
+    dept_no     CHAR(4)         NOT NULL,
+    dept_name   VARCHAR(40)     NOT NULL
+);
+
+INSERT INTO departments
+VALUES
+(
+	'd001',
+    'IT'
+);
+
+INSERT INTO departments
+VALUES
+(
+	'd002',
+    'CE'
+),
+(
+	'd003',
+    'Mech'
+);
+
+CREATE TABLE departments_dup (
+    dept_no     CHAR(4)         NOT NULL,
+    dept_name   VARCHAR(40)     NOT NULL
+);
+
+INSERT INTO  departments_dup
+(
+		dept_no,
+        dept_name
+)
+SELECT * FROM departments;
+
+SELECT*FROM departments_dup;
+
+
